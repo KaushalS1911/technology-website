2b4553a7606c32753a218daa3c47708404bb139b
Dax-savani <dakshsavani15@gmail.com>
2024-04-13 13:34:21 +0530

    compays added in home

--- src/pages/Home/Client.js
+++ src/pages/Home/Client.js
@@ -1,13 +1,26 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import company1 from "../../assets/images/home-image/company1.svg";
-import company2 from "../../assets/images/home-image/company2.svg";
-import company3 from "../../assets/images/home-image/company3.png";
-import company4 from "../../assets/images/home-image/company4.png";
-import company5 from "../../assets/images/home-image/company5.png";
+import company1 from "../../assets/images/home-image/companyLogo/company1.svg";
+import company2 from "../../assets/images/home-image/companyLogo/company2.svg";
+import company3 from "../../assets/images/home-image/companyLogo/company3.png";
+import company4 from "../../assets/images/home-image/companyLogo/company4.png";
+import company5 from "../../assets/images/home-image/companyLogo/company5.png";
+import company6 from "../../assets/images/home-image/companyLogo/downloadedLogo/astha2.png";
+import company7 from "../../assets/images/home-image/companyLogo/downloadedLogo/tenacious.png";
+import company8 from "../../assets/images/home-image/companyLogo/downloadedLogo/techstaunch.png";
+import company9 from "../../assets/images/home-image/companyLogo/downloadedLogo/tecocraft.png";
+import company10 from "../../assets/images/home-image/companyLogo/downloadedLogo/secretmindtech.png";
+import company11 from "../../assets/images/home-image/companyLogo/downloadedLogo/apc.png";
+import company12 from "../../assets/images/home-image/companyLogo/downloadedLogo/atologist.png";
+import company13 from "../../assets/images/home-image/companyLogo/downloadedLogo/weetech.png";
+import company14 from "../../assets/images/home-image/companyLogo/downloadedLogo/magnento.png";
+import company15 from "../../assets/images/home-image/companyLogo/downloadedLogo/zluck.png";
+import company16 from "../../assets/images/home-image/companyLogo/downloadedLogo/strong.png";
+import company17 from "../../assets/images/home-image/companyLogo/downloadedLogo/weingenious.png";
+import company18 from "../../assets/images/home-image/companyLogo/downloadedLogo/logistrix.png";
 import bglayer2 from "../../assets/images/home-image/bglayer2.png";
 
 const Client = () => {
@@ -37,19 +50,19 @@ const Client = () => {
     company3,
     company4,
     company5,
-    company1,
-    company2,
-    company3,
-    company4,
-    company5,
-    company1,
-    company2,
-    company3,
-    company4,
-    company5,
-    company1,
-    company2,
-    company3,
+    company6,
+    company7,
+    company8,
+    company9,
+    company10,
+    company11,
+    company12,
+    company13,
+    company14,
+    company15,
+    company16,
+    company17,
+    company18,
   ];
 
   return (
