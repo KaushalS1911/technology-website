2c21ecded44d0fb82cf0fa56f6b31d4087e5ae57
KaushalS <kaushalsojitra19@gmail.com>
2024-04-13 16:14:50 +0530

    - portfolio website project data changes

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
-import { useTheme } from "@emotion/react";
+import {Box, Button, Container, Grid, Typography} from "@mui/material";
+import {useTheme} from "@emotion/react";
 import nectarImg from "../../assets/images/portfolio/nectar-2 (1) 1.png";
 import nectarlogo from "../../assets/images/portfolio/Group 1 1.png";
-import nectarlogo2 from "../../assets/images/portfolio/Group 1 1 (1).png";
-
 import project1 from "../../assets/images/portfolio/project1 (1).png"
 import stocklogo from "../../assets/images/portfolio/sytock logo.png"
 import project2 from "../../assets/images/portfolio/stock.jpg";
@@ -12,212 +10,223 @@ import project3 from "../../assets/images/portfolio/project1 (4).png";
 import jbs from "../../assets/images/portfolio/jbs.jpg";
 import paperlogo from "../../assets/images/portfolio/paperlogo.png";
 import nectar from "../../assets/images/portfolio/nectar.jpg";
-import { useNavigate } from "react-router-dom";
-
+import {useNavigate} from "react-router-dom";
 
 
 const portfolioItems = [
-  {
-    id: 1,
-    img: nectarImg,
-    logo: nectarlogo,
-    title: "Nectar",
-    description:
-      "Fave is a fandom-centric community with highly engaging content where fans can create deeper and lasting connections. The vision to nurture and grow the community is centered on rewarding positive and meaningful engagement with points that can be redeemed for discounts and exclusive experiences.",
-    buttonVariant: "outlined",
-    buttonColor: "#fff",
-    buttonBorder: "white",
-    buttonBackground: "#fff",
-    backgroundColor: "rgba(91,212,129,1)",
-    sectionTextColor: "white",
-    images: [{nectarImg},{nectar}],
-  },
-  {
-    id: 2,
-    img: project2,
-    logo: paperlogo,
-    title: "Paper Generation",
-    description:
-      "Fave is a fandom-centric community with highly engaging content where fans can create deeper and lasting connections. The vision to nurture and grow the community is centered on rewarding positive and meaningful engagement with points that can be redeemed for discounts and exclusive experiences.",
-    buttonVariant: "outlined",
-    buttonColor: "rgba(91,212,129,1)",
-    buttonBorder: "rgba(91,212,129,1)",
-    buttonBackground: "rgba(91,212,129,1)",
-    backgroundColor: "#fff",
-    sectionTextColor: "#3E3E3E",
-  },
-  {
-    id: 3,
-    img: project1,
-    logo: stocklogo,
-    title: "Hedger Community",
-    description:
-      "Fave is a fandom-centric community with highly engaging content where fans can create deeper and lasting connections. The vision to nurture and grow the community is centered on rewarding positive and meaningful engagement with points that can be redeemed for discounts and exclusive experiences.",
-    buttonVariant: "outlined",
-    buttonColor: "#fff",
-    buttonBorder: "white",
-    buttonBackground: "#fff",
-    backgroundColor: "#141C25",
-    sectionTextColor: "white",
-  },
-  {
-    id: 4,
-    img: project3,
-    logo: jbs,
-    title: "Institute Website",
-    description:
-      "Fave is a fandom-centric community with highly engaging content where fans can create deeper and lasting connections. The vision to nurture and grow the community is centered on rewarding positive and meaningful engagement with points that can be redeemed for discounts and exclusive experiences.",
-    buttonVariant: "outlined",
-    buttonColor: "#141C25",
-    buttonBorder: "#141C25",
-    buttonBackground: "#141C25",
-    backgroundColor: "#fff",
-    sectionTextColor: "#3E3E3E",
-  },
+    {
+        id: 1,
+        img: nectarImg,
+        logo: nectarlogo,
+        title: "Nectar",
+        description:
+            "Welcome to Nectar, your one-stop destination for all your grocery needs! We offer a convenient online platform where you can browse through a wide selection of fresh produce, pantry essentials, household items, and more, all from the comfort of your home. With seamless ordering and secure payment options, we strive to make your shopping experience effortless and enjoyable. Whether you're looking for everyday staples or specialty ingredients, we've got you covered. Say goodbye to long queues and heavy bags – shop with us and have your groceries delivered straight to your doorstep, saving you time and hassle!",
+        buttonVariant: "outlined",
+        buttonColor: "#fff",
+        buttonBorder: "white",
+        buttonBackground: "#fff",
+        backgroundColor: "rgba(91,212,129,1)",
+        sectionTextColor: "white",
+        images: [{nectarImg}, {nectar}],
+    },
+    {
+        id: 2,
+        img: project2,
+        logo: paperlogo,
+        title: "Paper Generation",
+        description:
+            "Introducing our Paper Generation Application – your go-to tool for effortlessly creating custom academic papers. Specify your requirements, and within minutes, generate high-quality content tailored to your needs. Say goodbye to tedious writing processes and hello to streamlined productivity with our intuitive platform.",
+        buttonVariant: "outlined",
+        buttonColor: "rgba(91,212,129,1)",
+        buttonBorder: "rgba(91,212,129,1)",
+        buttonBackground: "rgba(91,212,129,1)",
+        backgroundColor: "#fff",
+        sectionTextColor: "#3E3E3E",
+    },
+    {
+        id: 3,
+        img: project1,
+        logo: stocklogo,
+        title: "Hedger Community",
+        description:
+            "Enter the world of online trading with Hadger Community. Access diverse investment opportunities, from stocks to cryptocurrencies, with real-time insights and expert support. Start trading smarter today.",
+        buttonVariant: "outlined",
+        buttonColor: "#fff",
+        buttonBorder: "white",
+        buttonBackground: "#fff",
+        backgroundColor: "#141C25",
+        sectionTextColor: "white",
+    },
+    {
+        id: 4,
+        img: project3,
+        logo: jbs,
+        title: "Institute Website",
+        description:
+            "Discover our Institute's website, your hub for academic excellence. Explore our courses, meet our faculty, and learn about our cutting-edge facilities. Start your journey towards success today.",
+        buttonVariant: "outlined",
+        buttonColor: "#141C25",
+        buttonBorder: "#141C25",
+        buttonBackground: "#141C25",
+        backgroundColor: "#fff",
+        sectionTextColor: "#3E3E3E",
+    },
 ];
 const Portfolio = () => {
-  const theme = useTheme();
-  const navigate = useNavigate()
-  return (
-    <>
-      <Box bgcolor={theme.palette.backgroundLiteGrey} sx={{ mt: "40px" }}>
-        <Box
-          sx={{ width: "100%" }}
-          px={{ xs: "2rem", sm: "4rem", md: "6rem", xl: "8rem" }}
-        >
-          <Grid>
-            <Grid item lg={9} xs={11} sx={{ py: { xs: 8, md: 13 } }}>
-              <Box
-                className="heading"
-                sx={{ fontSize: { md: "48px", xs: "32px" } }}
-              >
-                <Typography variant="div" fontWeight="bold">
-                  We take digital experiance to the <br />
-                  <Typography variant="span" color={theme.palette.primary.main}>
-                    next level
-                  </Typography>
-                </Typography>
-              </Box>
-              <Box className="service_paragraph" sx={{ my: "15px" }}>
-                <Typography>
-                  The products we build are changing the way indusrty leaders do
-                  thier core business
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      {portfolioItems?.map((item, index) => (
-        <Box
-          key={item.id}
-          bgcolor={item.backgroundColor}
-          color={item.sectionTextColor}
-        >
-          <Container maxWidth="lg">
-            <Box sx={{ py: "35px" }}>
-              <Grid container alignItems="center" spacing={5}>
-                {index % 2 === 0 && (
-                  <Grid
-                    item
-                    sx={{ order: { md: 0 } }}
-                    xs={12}
-                    md={6}
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <Box
-                      sx={{
-                        overflow: "hidden",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: { sm: "65%", md: "100%" },
-                      }}
-                    >
-                      <img src={item.img} alt={item.title} />
-                    </Box>
-                  </Grid>
-                )}
-                <Grid item xs={12} md={6} sx={{ order: { md: 0, xs: 1 } }}>
-                  <Box sx={{ width: { md: "270px", xs: "200px" } }}>
-                    <img src={item.logo} alt="logo" />
-                  </Box>
-                  <Typography
-                    variant="h4"
-                    sx={{
-                      fontSize: { md: "42px", xs: "32px" },
-                      fontWeight: "600",
-                      marginTop: "15px",
-                      letterSpacing: "1.2px",
-                    }}
-                  >
-                    {item.title}
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    sx={{
-                      mt: "20px",
-                      fontWeight: "600",
-                      lineHeight: "1.5",
-                      letterSpacing: "0.9px",
-                    }}
-                  >
-                    {item.description}
-                  </Typography>
-                  <Button
-                    variant={item.buttonVariant}
-                    sx={{
-                      mt: 3,
-                      borderRadius: "30px",
-                      fontWeight: "600",
-                      mt: "30px",
-                      padding: "16px 58px",
-                      borderColor: item.buttonBorder,
-                      color: item.buttonColor,
-                      backgroundColor: item.backgroundColor,
-                      "&:hover": {
-                        backgroundColor: item.buttonBackground,
-                        borderColor: item.buttonBorder,
-                        color: item.backgroundColor,
-                      },
-                    }}
-                    onClick={() =>
-                      navigate(`/portfolio/single-project/${item.id}`)
-                    }
-                  >
-                    View more
-                  </Button>
-                </Grid>
-                {index % 2 !== 0 && (
-                  <Grid
-                    item
-                    sx={{ order: { md: 0 } }}
-                    xs={12}
-                    md={6}
-                    display={"flex"}
-                    justifyContent={"center"}
-                  >
-                    <Box
-                      sx={{
-                        overflow: "hidden",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: { sm: "65%", md: "100%" },
-                      }}
-                    >
-                      <img src={item.img} alt={item.title} />
-                    </Box>
-                  </Grid>
-                )}
-              </Grid>
+    const theme = useTheme();
+    const navigate = useNavigate()
+    return (
+        <>
+            <Box bgcolor={theme.palette.backgroundLiteGrey} sx={{mt: "40px"}}>
+                <Box
+                    sx={{width: "100%"}}
+                    px={{xs: "2rem", sm: "4rem", md: "6rem", xl: "8rem"}}
+                >
+                    <Grid>
+                        <Grid item lg={9} xs={11} sx={{py: {xs: 8, md: 13}}}>
+                            <Box
+                                className="heading"
+                                sx={{fontSize: {md: "48px", xs: "32px"}}}
+                            >
+                                <Typography variant="div" fontWeight="bold">
+                                    We take digital experience to the <br/>
+                                    <Typography variant="span"
+                                                color={theme.palette.primary.main}>
+                                        next level
+                                    </Typography>
+                                </Typography>
+                            </Box>
+                            <Box className="service_paragraph"
+                                 sx={{my: "15px"}}>
+                                <Typography>
+                                    The products we build are changing the way
+                                    indusrty leaders do
+                                    thier core business
+                                </Typography>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Box>
             </Box>
-          </Container>
-        </Box>
-      ))}
-    </>
-  );
+            {portfolioItems?.map((item, index) => (
+                <Box
+                    key={item.id}
+                    bgcolor={item.backgroundColor}
+                    color={item.sectionTextColor}
+                >
+                    <Container maxWidth="lg">
+                        <Box sx={{py: "35px"}}>
+                            <Grid container alignItems="center" spacing={5}>
+                                {index % 2 === 0 && (
+                                    <Grid
+                                        item
+                                        sx={{order: {md: 0}}}
+                                        xs={12}
+                                        md={6}
+                                        display={"flex"}
+                                        justifyContent={"center"}
+                                    >
+                                        <Box
+                                            sx={{
+                                                overflow: "hidden",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                                width: {sm: "65%", md: "100%"},
+                                            }}
+                                        >
+                                            <img src={item.img}
+                                                 alt={item.title}/>
+                                        </Box>
+                                    </Grid>
+                                )}
+                                <Grid item xs={12} md={6}
+                                      sx={{order: {md: 0, xs: 1}}}>
+                                    <Box sx={{
+                                        width: {
+                                            md: "270px",
+                                            xs: "200px"
+                                        }
+                                    }}>
+                                        <img src={item.logo} alt="logo"/>
+                                    </Box>
+                                    <Typography
+                                        variant="h4"
+                                        sx={{
+                                            fontSize: {md: "42px", xs: "32px"},
+                                            fontWeight: "600",
+                                            marginTop: "15px",
+                                            letterSpacing: "1.2px",
+                                        }}
+                                    >
+                                        {item.title}
+                                    </Typography>
+                                    <Typography
+                                        variant="body1"
+                                        sx={{
+                                            mt: "20px",
+                                            fontWeight: "600",
+                                            lineHeight: "1.5",
+                                            letterSpacing: "0.9px",
+                                        }}
+                                    >
+                                        {item.description}
+                                    </Typography>
+                                    <Button
+                                        variant={item.buttonVariant}
+                                        sx={{
+                                            mt: 3,
+                                            borderRadius: "30px",
+                                            fontWeight: "600",
+                                            mt: "30px",
+                                            padding: "16px 58px",
+                                            borderColor: item.buttonBorder,
+                                            color: item.buttonColor,
+                                            backgroundColor: item.backgroundColor,
+                                            "&:hover": {
+                                                backgroundColor: item.buttonBackground,
+                                                borderColor: item.buttonBorder,
+                                                color: item.backgroundColor,
+                                            },
+                                        }}
+                                        onClick={() =>
+                                            navigate(`/portfolio/single-project/${item.id}`)
+                                        }
+                                    >
+                                        View more
+                                    </Button>
+                                </Grid>
+                                {index % 2 !== 0 && (
+                                    <Grid
+                                        item
+                                        sx={{order: {md: 0}}}
+                                        xs={12}
+                                        md={6}
+                                        display={"flex"}
+                                        justifyContent={"center"}
+                                    >
+                                        <Box
+                                            sx={{
+                                                overflow: "hidden",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                                width: {sm: "65%", md: "100%"},
+                                            }}
+                                        >
+                                            <img src={item.img}
+                                                 alt={item.title}/>
+                                        </Box>
+                                    </Grid>
+                                )}
+                            </Grid>
+                        </Box>
+                    </Container>
+                </Box>
+            ))}
+        </>
+    );
 };
-export { portfolioItems };
+
+export {portfolioItems};
 export default Portfolio;
