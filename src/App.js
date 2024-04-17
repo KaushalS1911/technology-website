17679518c403e3d4b47e6838a52ae57e87c4f81c
KaushalS <kaushalsojitra19@gmail.com>
2024-04-17 10:14:49 +0530

    - scroll issue fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Portfolio from "./pages/Portfolio/Portfolio";
-import { Services } from "./pages/services/Services";
-import { Careers } from "./pages/careers/Careers";
-import { Route, Routes } from "react-router-dom";
-import { CareerCardData } from "./pages/careers/CareerCardData";
+import {Services} from "./pages/services/Services";
+import {Careers} from "./pages/careers/Careers";
+import {Route, Routes, useLocation} from "react-router-dom";
+import {CareerCardData} from "./pages/careers/CareerCardData";
 // import Home from "./pages/Home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/Contact Us/Contact";
@@ -13,25 +13,38 @@ import Home from "./pages/Home/Home";
 import SingleProject from "./pages/Portfolio/SingleProject";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route
-          path="/portfolio/single-project/:id"
-          element={<SingleProject />}
-        />
-        <Route path="/services" element={<Services />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/career-card-data/:id" element={<CareerCardData />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+
+    function ScrollToTop() {
+        const {pathname} = useLocation();
+
+        useEffect(() => {
+            window.scrollTo(0, 0);
+        }, [pathname]);
+
+        return null;
+    }
+
+    return (
+        <>
+            <Header/>
+            <ScrollToTop/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/portfolio" element={<Portfolio/>}/>
+                <Route
+                    path="/portfolio/single-project/:id"
+                    element={<SingleProject/>}
+                />
+                <Route path="/services" element={<Services/>}/>
+                <Route path="/careers" element={<Careers/>}/>
+                <Route path="/career-card-data/:id"
+                       element={<CareerCardData/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+            </Routes>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
